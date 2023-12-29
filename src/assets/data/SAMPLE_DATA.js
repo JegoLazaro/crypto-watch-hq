dd8d4d1085cdf535f2d9386953b972d0ff1ab04b
JegoLazaro <jose_gabriel_lazaro@dlsu.edu.ph>
2023-12-29 20:36:29 +0800

    style: edited table sapcing and configured api details

---
+++ src/assets/data/SAMPLE_DATA.js
@@ -0,0 +1,213 @@
+export const SAMPLE_DATA =[
+    {
+      "image": "https://example.com/coin1.png",
+      "name": "Bitcoin",
+      "symbol": "BTC",
+      "current_price": 50000.5,
+      "market_cap": 950000000000
+    },
+    {
+      "image": "https://example.com/coin2.png",
+      "name": "Ethereum",
+      "symbol": "ETH",
+      "current_price": 3000.75,
+      "market_cap": 350000000000
+    },
+    {
+      "image": "https://example.com/coin3.png",
+      "name": "Cardano",
+      "symbol": "ADA",
+      "current_price": 2.45,
+      "market_cap": 80000000000
+    },
+    {
+      "image": "https://example.com/coin4.png",
+      "name": "Binance Coin",
+      "symbol": "BNB",
+      "current_price": 400.25,
+      "market_cap": 70000000000
+    },
+    {
+      "image": "https://example.com/coin5.png",
+      "name": "Ripple",
+      "symbol": "XRP",
+      "current_price": 1.1,
+      "market_cap": 50000000000
+    },
+    {
+      "image": "https://example.com/coin6.png",
+      "name": "Litecoin",
+      "symbol": "LTC",
+      "current_price": 150.8,
+      "market_cap": 10000000000
+    },
+    {
+      "image": "https://example.com/coin7.png",
+      "name": "Polkadot",
+      "symbol": "DOT",
+      "current_price": 30.0,
+      "market_cap": 15000000000
+    },
+    {
+      "image": "https://example.com/coin8.png",
+      "name": "Chainlink",
+      "symbol": "LINK",
+      "current_price": 25.5,
+      "market_cap": 8000000000
+    },
+    {
+      "image": "https://example.com/coin9.png",
+      "name": "Stellar",
+      "symbol": "XLM",
+      "current_price": 0.45,
+      "market_cap": 5000000000
+    },
+    {
+      "image": "https://example.com/coin10.png",
+      "name": "Dogecoin",
+      "symbol": "DOGE",
+      "current_price": 0.3,
+      "market_cap": 4000000000
+    },
+    {
+      "image": "https://example.com/coin11.png",
+      "name": "Cardano",
+      "symbol": "ADA",
+      "current_price": 2.45,
+      "market_cap": 80000000000
+    },
+    {
+      "image": "https://example.com/coin12.png",
+      "name": "Binance Coin",
+      "symbol": "BNB",
+      "current_price": 400.25,
+      "market_cap": 70000000000
+    },
+    {
+      "image": "https://example.com/coin13.png",
+      "name": "Ripple",
+      "symbol": "XRP",
+      "current_price": 1.1,
+      "market_cap": 50000000000
+    },
+    {
+      "image": "https://example.com/coin14.png",
+      "name": "Litecoin",
+      "symbol": "LTC",
+      "current_price": 150.8,
+      "market_cap": 10000000000
+    },
+    {
+      "image": "https://example.com/coin15.png",
+      "name": "Polkadot",
+      "symbol": "DOT",
+      "current_price": 30.0,
+      "market_cap": 15000000000
+    },
+    {
+      "image": "https://example.com/coin16.png",
+      "name": "Chainlink",
+      "symbol": "LINK",
+      "current_price": 25.5,
+      "market_cap": 8000000000
+    },
+    {
+      "image": "https://example.com/coin17.png",
+      "name": "Stellar",
+      "symbol": "XLM",
+      "current_price": 0.45,
+      "market_cap": 5000000000
+    },
+    {
+      "image": "https://example.com/coin18.png",
+      "name": "Dogecoin",
+      "symbol": "DOGE",
+      "current_price": 0.3,
+      "market_cap": 4000000000
+    },
+    {
+      "image": "https://example.com/coin19.png",
+      "name": "Cardano",
+      "symbol": "ADA",
+      "current_price": 2.45,
+      "market_cap": 80000000000
+    },
+    {
+      "image": "https://example.com/coin20.png",
+      "name": "Binance Coin",
+      "symbol": "BNB",
+      "current_price": 400.25,
+      "market_cap": 70000000000
+    },
+    {
+      "image": "https://example.com/coin21.png",
+      "name": "Ripple",
+      "symbol": "XRP",
+      "current_price": 1.1,
+      "market_cap": 50000000000
+    },
+    {
+      "image": "https://example.com/coin22.png",
+      "name": "Litecoin",
+      "symbol": "LTC",
+      "current_price": 150.8,
+      "market_cap": 10000000000
+    },
+    {
+      "image": "https://example.com/coin23.png",
+      "name": "Polkadot",
+      "symbol": "DOT",
+      "current_price": 30.0,
+      "market_cap": 15000000000
+    },
+    {
+      "image": "https://example.com/coin24.png",
+      "name": "Chainlink",
+      "symbol": "LINK",
+      "current_price": 25.5,
+      "market_cap": 8000000000
+    },
+    {
+      "image": "https://example.com/coin25.png",
+      "name": "Stellar",
+      "symbol": "XLM",
+      "current_price": 0.45,
+      "market_cap": 5000000000
+    },
+    {
+      "image": "https://example.com/coin26.png",
+      "name": "Dogecoin",
+      "symbol": "DOGE",
+      "current_price": 0.3,
+      "market_cap": 4000000000
+    },
+    {
+      "image": "https://example.com/coin27.png",
+      "name": "Cardano",
+      "symbol": "ADA",
+      "current_price": 2.45,
+      "market_cap": 80000000000
+    },
+    {
+      "image": "https://example.com/coin28.png",
+      "name": "Binance Coin",
+      "symbol": "BNB",
+      "current_price": 400.25,
+      "market_cap": 70000000000
+    },
+    {
+      "image": "https://example.com/coin29.png",
+      "name": "Ripple",
+      "symbol": "XRP",
+      "current_price": 1.1,
+      "market_cap": 50000000000
+    },
+    {
+      "image": "https://example.com/coin30.png",
+      "name": "Litecoin",
+      "symbol": "LTC",
+      "current_price": 150.8,
+      "market_cap": 10000000000
+    }
+  ]
+  
--- src/components/Market.jsx
+++ src/components/Market.jsx
@@ -1,67 +1,69 @@
 import React, { useState, useEffect, useMemo } from "react";
 import axios from "axios";
 import Pagination from "./Pagination";
+import { SAMPLE_DATA } from "../assets/data/SAMPLE_DATA";
 
 const Market = () => {
   const [data, setData] = useState([]);
   const [loading, setLoading] = useState(true);
 
-  const SAMPLE_DATA = [
-    {
-      id: "bitcoin",
-      symbol: "btc",
-      name: "Bitcoin",
-      image:
-        "https://assets.coingecko.com/coins/images/1/large/bitcoin.png?1547033579",
-      current_price: "Loading...",
-    },
-    {
-      id: "etheruem",
-      symbol: "eth",
-      name: "Etheruem",
-      image:
-        "https://assets.coingecko.com/coins/images/279/large/ethereum.png?1595348880",
-      current_price: "Loading...",
-    },
-    {
-      id: "tether",
-      symbol: "usdt",
-      name: "Tether",
-      image:
-        "https://assets.coingecko.com/coins/images/325/large/Tether-logo.png?1598003707",
-      current_price: "Loading...",
-    },
-    {
-      id: "binance coin",
-      symbol: "bnb",
-      name: "Binance Coin",
-      image:
-        "https://assets.coingecko.com/coins/images/825/large/binance-coin-logo.png?1547034615",
-      current_price: "Loading...",
-    },
-    {
-      id: "cardano",
-      symbol: "ada",
-      name: "Cardano",
-      image:
-        "https://assets.coingecko.com/coins/images/975/large/cardano.png?1547034860",
-      current_price: "Loading...",
-    },
-    {
-      id: "ripple",
-      symbol: "xrp",
-      name: "XRP",
-      image:
-        "https://assets.coingecko.com/coins/images/44/large/xrp-symbol-white-128.png?1605778731",
-      current_price: "Loading...",
-    },
-  ];
+  //   const SAMPLE_DATA = [
+  //     {
+  //       id: "bitcoin",
+  //       symbol: "btc",
+  //       name: "Bitcoin",
+  //       image:
+  //         "https://assets.coingecko.com/coins/images/1/large/bitcoin.png?1547033579",
+  //       current_price: "Loading...",
+  //     },
+  //     {
+  //       id: "etheruem",
+  //       symbol: "eth",
+  //       name: "Etheruem",
+  //       image:
+  //         "https://assets.coingecko.com/coins/images/279/large/ethereum.png?1595348880",
+  //       current_price: "Loading...",
+  //     },
+  //     {
+  //       id: "tether",
+  //       symbol: "usdt",
+  //       name: "Tether",
+  //       image:
+  //         "https://assets.coingecko.com/coins/images/325/large/Tether-logo.png?1598003707",
+  //       current_price: "Loading...",
+  //     },
+  //     {
+  //       id: "binance coin",
+  //       symbol: "bnb",
+  //       name: "Binance Coin",
+  //       image:
+  //         "https://assets.coingecko.com/coins/images/825/large/binance-coin-logo.png?1547034615",
+  //       current_price: "Loading...",
+  //     },
+  //     {
+  //       id: "cardano",
+  //       symbol: "ada",
+  //       name: "Cardano",
+  //       image:
+  //         "https://assets.coingecko.com/coins/images/975/large/cardano.png?1547034860",
+  //       current_price: "Loading...",
+  //     },
+  //     {
+  //       id: "ripple",
+  //       symbol: "xrp",
+  //       name: "XRP",
+  //       image:
+  //         "https://assets.coingecko.com/coins/images/44/large/xrp-symbol-white-128.png?1605778731",
+  //       current_price: "Loading...",
+  //     },
+  //   ];
   useEffect(() => {
     axios
       .get(
-        "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=50&page=1&sparkline=true&price_change_percentage=7d&locale=en"
+        "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=40&page=1&sparkline=true&price_change_percentage=7d&locale=en"
       )
       .then(({ data: responseData }) => {
+        // console.log('data=>> ',data);
         setData(responseData);
         setLoading(false);
       })
@@ -78,43 +80,55 @@ const Market = () => {
   const currentTableData = useMemo(() => {
     const firstPageIndex = (currentPage - 1) * PageSize;
     const lastPageIndex = firstPageIndex + PageSize;
+    console.log("DATA =>> ", data);
     return data.slice(firstPageIndex, lastPageIndex);
   }, [currentPage]);
   return (
     <div className="px-6 pt-14 lg:px-8 py-5 bg-gray-800">
-      <h1 className="text-white text-4xl p-5">Market</h1>
-      <div className="py-10 max-w-6xl mx-auto flex px-5 flex-row flex-wrap items-center bg-slate-600 ">
-        <table className="border-2 w-full border-blue-500 ">
+      <h1 className="text-white text-5xl p-5 justify-center max-w-7xl mx-auto tracking-wide font-bold">
+        Market
+      </h1>
+      <div className="py-10 max-w-6xl mx-auto flex px-5 flex-row flex-wrap items-center bg-gray-700 rounded-xl border-4 border-green-700">
+        <table className="w-full border-green-700  mb-5">
           <thead>
-            <tr className="text-left ">
-              <th className="pl-3 text-2xl">Coin</th>
-              <th className="pl-3 text-2xl">Price</th>
-              <th className="pl-2 text-2xl">7d Per Change</th>
-              <th className="pl-3 text-2xl">Market Cap</th>
+            <tr className="text-left border-b-4 border-green-500">
+              <th className="pl-3 text-2xl text-green-500">Coin</th>
+              <th className="pl-3 text-2xl text-gray-300">Price</th>
+              <th className="pl-2 text-2xl text-gray-300">7d</th>
+              <th className="pl-3 text-2xl text-gray-300">Market Cap</th>
             </tr>
           </thead>
+
           <tbody>
             {currentTableData.map((item) => {
               return (
-                <tr className="">
-                  <td className="flex flex-row p-2">
+                <tr className="hover:bg-gray-400 border-b-4 cursor-pointer text-white">
+                  <td className="flex flex-row p-2 ">
                     <img src={item.image} className="w-10 h-auto" />
-                    <h1 className="px-3 self-center">
+                    <h1 className="px-3 self-center text-lg font-semibold">
                       {item.name} ({item.symbol.toUpperCase()})
                     </h1>
                   </td>
-                  <td>
-                  $ {item.current_price.toLocaleString('en-US', { currency: 'USD'})}
-                </td>
-                  <td>{item.name}</td>
-                  <td>$ {item.market_cap.toLocaleString('en-US', { currency: 'USD'})}</td>
+                  <td className="text-lg font-semibold tracking-wide">
+                    ${" "}
+                    {item.current_price.toLocaleString("en-US", {
+                      currency: "USD",
+                    })}
+                  </td>
+                  <td>{item.market_cap_change_percentage_24h}</td>
+                  <td className=" tracking-wide">
+                    ${" "}
+                    {item.market_cap.toLocaleString("en-US", {
+                      currency: "USD",
+                    })}
+                  </td>
                 </tr>
               );
             })}
           </tbody>
         </table>
         <Pagination
-          className="pagination-bar"
+          className="pagination-bar mx-auto "
           currentPage={currentPage}
           totalCount={data.length}
           pageSize={PageSize}
